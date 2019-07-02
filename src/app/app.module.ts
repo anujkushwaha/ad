620990305e41b94ec6c7ec1bb505e75f9bbba109
anujkushwaha <anuj.kushwaha@udmglobal.com>
2019-07-02 13:18:45 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,101 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+declare var $: any;
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+  menuArr: MainArrayType[]=[];
+  dashboardMainMenu;
+
+  title = 'difuze dashborad';
+
+  permissions : any[] = [
+    "ET Details View",
+    "ET Search",
+    "ET Location",
+    "ET Template View",
+    "User Permission View",
+    "WF View",
+    "WF Operator",
+    "Notification View",
+    "TM Admin",
+    "TM View",
+    "Title View",
+    "TS Admin",
+    "TS View",
+    "TS Operator",
+    "CDN View",
+    "Folder View",
+    "QueryViewer",
+    "Metadata View",
+    "Metadata Model Admin",
+    "Metadata Admin",
+    "Bulk Order Operator",
+    "Bulk Order Admin",
+    "notify",
+    "bop",
+    "abc1",
+    "abc2",
+    "abc3",
+    "abc4",
+    "abc5",
+    "abc6",
+    "abc7",
+    "abc8",
+    "abc9",
+    "abc10",
+    "abc11",
+    "abc12",
+  ];   
+
+  constructor(private httpService: HttpClient) { }  
+   
+  ngOnInit() {
+    this.httpService.get('./assets/header.json').subscribe(
+        (data) => {
+          //debugger;
+          this.dashboardMainMenu = data;
+
+          // for(let x of this.dashboardMainMenu){
+          //   this.menuArr.push(x.child); 
+          // } 
+
+          for(let schild of this.dashboardMainMenu){
+            this.menuArr.push(schild.child); 
+            if(schild.child && schild.child.length>0){
+              schild.child.forEach((x:any)=>{
+                if(this.permissions.includes(x.Permission))
+                {
+                  x.Visible='true';
+                  schild.Visible='true';
+                }
+              });
+            }
+            if(this.permissions.includes(schild.Permission)){
+              schild.Visible = 'true';
+            }
+          } 
+        },        
+
+        (err: HttpErrorResponse) => {
+          console.log (err.message);
+        },
+
+        () => {
+        }  
+    );
+  }
+}
+export class MainArrayType{ 
+  applicationName: string;
+  url: string;
+}
+
+
+
+
