317c56621427e3f25df99fa4854b836efcef030e
anujkushwaha <anuj.kushwaha@udmglobal.com>
2019-07-05 17:23:34 +0530

    New Chnagess

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { HttpErrorResponse } from "@angular/common/http";
 declare var $: any;
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.scss"]
 })
 export class DashboardComponent implements OnInit {
-  menuArr: MainArrayType[]=[];
+  menuArr: MainArrayType[] = [];
   dashboardMainMenu;
+  test: boolean = false;
+  sep : any;
 
-  title = 'difuze dashborad';
+  title = "difuze dashborad";
 
-  permissions : any[] = [
+  permissions: any[] = [
     "ET Details View",
     "ET Search",
     "ET Location",
@@ -50,52 +52,44 @@ export class DashboardComponent implements OnInit {
     "abc9",
     "abc10",
     "abc11",
-    "abc12",
-  ];   
+    "abc12"
+  ];
 
-  constructor(private httpService: HttpClient) { }  
-   
+  constructor(private httpService: HttpClient) {}
   ngOnInit() {
-    this.httpService.get('./assets/header.json').subscribe(
-        (data) => {
-          //debugger;
-          this.dashboardMainMenu = data;
+    this.httpService.get("./assets/header.json").subscribe(
+      data => {
+        //debugger;
+        this.dashboardMainMenu = data;
+        this.sep = this.dashboardMainMenu.length - 2;
 
-          // for(let x of this.dashboardMainMenu){
-          //   this.menuArr.push(x.child); 
-          // } 
+        for (let schild of this.dashboardMainMenu) {
+          this.menuArr.push(schild.child);
+          if (schild.child && schild.child.length > 0) {
+            schild.child.forEach((x: any) => {
+              if (this.permissions.includes(x.Permission)) {
+                x.Visible = "true";
+                schild.Visible = "true";
+              }
+            });
+          }
+          if (this.permissions.includes(schild.Permission)) {
+            schild.Visible = "true";
+          }
+        }
+      },
 
-          for(let schild of this.dashboardMainMenu){
-            this.menuArr.push(schild.child); 
-            if(schild.child && schild.child.length>0){
-              schild.child.forEach((x:any)=>{
-                if(this.permissions.includes(x.Permission))
-                {
-                  x.Visible='true';
-                  schild.Visible='true';
-                }
-              });
-            }
-            if(this.permissions.includes(schild.Permission)){
-              schild.Visible = 'true';
-            }
-          } 
-        },        
+      (err: HttpErrorResponse) => {
+        console.log(err.message);
+      },
 
-        (err: HttpErrorResponse) => {
-          console.log (err.message);
-        },
-
-        () => {
-        }  
+      () => {
+        
+      }
     );
   }
 }
-export class MainArrayType{ 
+export class MainArrayType {
   applicationName: string;
   url: string;
 }
-
-
-
-
